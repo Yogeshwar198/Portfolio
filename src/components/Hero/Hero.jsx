fefcb5af35750937e12a216723075dc875e54cd8
Yogeshwar198 <yogeshwaredu198@gmail.com>
2024-08-13 14:06:09 +0530

    first commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
             </div>
             <h1><span>I'm Yogeshwar Singh,</span> Frontend developer based in India.</h1>
             <p>
-                I am a MERN developer from India, eager to start my career and gain experience in a dynamic and innovative company.</p>
+                I am a Frontend developer from India, eager to start my career and gain experience in a dynamic and innovative company.</p>
             <div className="hero-action">
                 <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
              <a href={Resume}  className="hero-resume">My resume</a>
